f43260ca2c19641dcf6018418720fc2491c620a9
Elizabeth Lee <dlekwjd347@gmail.com>
2020-08-15 17:44:12 -0400

    delete function, api routes, and html routes

---
+++ routes/apiRoutes.js
@@ -0,0 +1,64 @@
+// ===============================================================================
+// LOAD DATA
+// We are linking our routes to a series of "data" sources.
+// These data sources hold arrays of information on table-data, waitinglist, etc.
+// ===============================================================================
+
+let noteData = require("../db/db.json");
+
+// ===============================================================================
+// ROUTING
+// ===============================================================================
+
+module.exports = function (app) {
+  // API GET Requests
+  // ---------------------------------------------------------------------------
+
+  app.get("/api/notes", function (req, res) {
+    res.json(noteData);
+  });
+
+  app.post("/api/notes", function (req, res) {
+
+    let noteId = uuid();
+    let newNote = {
+      id: noteId,
+      title: req.body.title,
+      text: req.body.text
+    };
+
+    fs.readFile("../db/db.json", "utf-8", (err, data) => {
+      if (err) throw err;
+      console.log(data);
+    });
+
+    const notesInput = JSON.parse(data);
+    notesInput.push(newNote)
+
+    fs.writeFile('../db/db.json', JSON.stringify(notesInput, null, 2), (err) => {
+      if (err) throw err;
+      res.send(noteData);
+      console.log('The note has been saved!');
+    });
+
+  })
+  fs.writeFileSync(noteData, notesInput, 'utf-8')
+
+  app.delete("/api/notes/:id", function (req, res) {
+    //  below to get to the ID and reference it
+    let noteId = req.params.id;
+
+    fs.readFile("./db/db.json", "utf8", (err, data) => {
+      if (err) throw err;
+      const notesInput = JSON.parse(data);
+      let noteData = noteData.filter((e, i) => i != noteId);
+   
+      fs.writeFile("./db/db.json", JSON.stringify(noteData, null, 2), err => {
+        if (err) throw err;
+        res.send(noteData)
+        console.log("The note has been deleted!");
+      });
+    })
+  })
+
+  };
---
+++ server.js
@@ -0,0 +1,22 @@
+const express = require("express");
+const fs = require("fs");
+const path = require('path');
+const uuid = require("uuidv4");
+
+// Initialize express app
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+// Setup data parsing
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+app.use(express.static(__dirname + "/public"));
+
+//Require routes file
+require('./routes/apiRoutes')(app);
+require('./routes/htmlRoutes')(app);
+
+// Setup listener
+app.listen(PORT, function () {
+    console.log("App listening on PORT: " + PORT);
+}); 
