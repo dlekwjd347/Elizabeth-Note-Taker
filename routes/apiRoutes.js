37ecf08133794abdb236c07bc71cbacda01ffe79
Elizabeth Lee <dlekwjd347@gmail.com>
2020-08-17 23:05:59 -0400

    file paths working!

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -1,64 +1,59 @@
 // ===============================================================================
 // LOAD DATA
-// We are linking our routes to a series of "data" sources.
-// These data sources hold arrays of information on table-data, waitinglist, etc.
+// We are linking our routes to a series of "data" sources
 // ===============================================================================
 
-let noteData = require("../db/db.json");
+let notesData = require("../db/db.json");
 const fs = require("fs");
-const uuid = require("uuidv4");
+const { uuid } = require("uuidv4");
 const path = require("path");
-
 
 // ===============================================================================
 // ROUTING
 // ===============================================================================
 
 module.exports = function (app) {
-  // API GET Requests
-  // ---------------------------------------------------------------------------
 
-  app.get("/api/notes", function (req, res) {
-    res.json(noteData);
-  });
+    app.get("/api/notes", function (req, res) {
+        res.json(notesData);
+    });
 
-  app.post("/api/notes", function (req, res) {
+    app.post("/api/notes", function (req, res) {
+        let noteID = uuid();
+        let newNote = {
+            id: noteID,
+            title: req.body.title,
+            text: req.body.text
+        };
 
-    let noteId = uuid();
-    let newNote = {
-      id: noteId,
-      title: req.body.title,
-      text: req.body.text
-    };
+        notesData.push(newNote);
 
-    notesData.push(newNote);
-
-    fs.writeFile(path.join(__dirname, "../db/db.json"), JSON.stringify(notesData), err => {
-      if (err) { console.log(err) }
-      res.send(newNote);
-      console.log("The note has been saved!");
-    
-  })
-
-  app.delete("/api/notes/:id", (req, res) => {
-    let noteID = req.params.id;
-
-    fs.readFile(path.join(__dirname, "../db/db.json"), "utf8", (err, data) => {
-        if (err) { console.log(err) }
-        const notesInput = JSON.parse(data);
-        const filterNotes = notesInput.filter((note) => {
-            return note.id !== noteID
-        });
-
-
-        fs.writeFile(path.join(__dirname, "../db/db.json"), JSON.stringify(filterNotes), err => {
+        fs.writeFile(path.join(__dirname, "../db/db.json"), JSON.stringify(notesData), err => {
             if (err) { console.log(err) }
-            console.log(filterNotes);
-            res.send(filterNotes);
-            console.log("The note has been deleted!")
-      });
-    })
-  })
+            res.send(newNote);
+            console.log("The Note has been saved!");
 
-})
-}
+        });
+    });
+
+
+    app.delete("/api/notes/:id", (req, res) => {
+        let noteID = req.params.id;
+
+        fs.readFile(path.join(__dirname, "../db/db.json"), "utf8", (err, data) => {
+            if (err) { console.log(err) }
+            let notesInput = JSON.parse(data);
+            notesData = notesInput.filter((note) => {
+                return note.id !== noteID
+            });
+
+
+            fs.writeFile(path.join(__dirname, "../db/db.json"), JSON.stringify(notesData), err => {
+                if (err) { console.log(err) }
+                console.log(notesData);
+                res.send(notesData);
+                console.log("The note has been deleted!")
+            });
+        });
+    });
+};
--- server.js
+++ server.js
@@ -1,21 +1,16 @@
 const express = require("express");
-const fs = require("fs");
 const path = require('path');
-const uuid = require("uuidv4");
-
+const uuid = require('uuidv4');
 // Initialize express app
 const app = express();
 const PORT = process.env.PORT || 3000;
-
 // Setup data parsing
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
-app.use(express.static("/public"));
-
+app.use(express.static("public"));
 //Require routes file
 require('./routes/apiRoutes')(app);
 require('./routes/htmlRoutes')(app);
-
 // Setup listener
 app.listen(PORT, function () {
     console.log("App listening on PORT: " + PORT);
